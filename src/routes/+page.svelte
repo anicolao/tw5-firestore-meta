592b5848799fa693d734c2f7bdf51b27a9d6ab68
Alex Nicolaou <alex@stockgamblers.com>
2023-10-02 02:53:35 -0400

    Deployable/usable tiddlywiki shell.
    This shell extends the proof of concept by writing the firebase
    configuration to the correct spot in localstorage, and serving
    all URLs under /tw/[name] as a tiddlywiki. Then the static build
    served for /tw/poc for example looks in localstorage for the
    firestore config, and if it finds it works against that firestore
    instance.
    
    Meanwhile, since the configuration for the wide open proof of
    concept is hardcoded into the top level landing page, anyone can
    click the proof of concept link and see and later edit the wiki.
    
    Firestore's support for svelte works fine, but I am not sure about
    using a cloud function for SSR. It is definitely slow the first time
    and doesn't yet provide any advantage.
    
    Firestore also has some sort of node engine mismatch bug which can
    be worked around by modifying the .npmrc to not enforce strict
    checking that the local node and the node expected by functions
    is identical.
    
    But by and large, this is working great and just needs programmatic
    creation of firestore instances and the ability to write those to
    local storage added to make it a fully functioning wiki creation
    and sharing server.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { page } from "$app/stores";
+  console.log(`Sveltekit script for ${$page.url}`);
+  try {
+    const s = window.localStorage;
+    const key = `$:/FirebaseConfig|${$page.url}tw/poc`;
+    const pocConfig = {
+      apiKey: "AIzaSyAAU8G6_I93RuQsfFdOf5wwdU4Wpn3cTXk",
+      authDomain: "tiddlywiki-a94cd.firebaseapp.com",
+      projectId: "tiddlywiki-a94cd",
+      storageBucket: "tiddlywiki-a94cd.appspot.com",
+      messagingSenderId: "251419323197",
+      appId: "1:251419323197:web:0e0ee30112d98099857354",
+    };
+    const config: string | null = s.getItem(key);
+    if (!config || config.length < 5) {
+      s.setItem(key, JSON.stringify(pocConfig));
+      console.log(
+        `Set missing localstorage config ${JSON.stringify(pocConfig)}`
+      );
+    }
+  } catch (err) {
+    console.error(err);
+  }
+</script>
+
+<h1>Welcome to Firestore backed TiddlyWiki5</h1>
+
+<p>Go to the <a href="/tw/poc">Proof of Concept</a>.</p>
